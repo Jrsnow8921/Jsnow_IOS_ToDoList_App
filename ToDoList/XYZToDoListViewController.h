b2fd7656d79e4f06d16d26348c88fe1386d10d32
Justin <jrsnow8921@gmail.com>
2015-07-23 08:52:27 -0400

    Update

--- ToDoList/XYZToDoListViewController.h
+++ ToDoList/XYZToDoListViewController.h
@@ -3,8 +3,8 @@
 //  ToDoList
 //
 //  Created by Justin Snow on 02/07/15.
-//  Copyright (c) 2015 Justin Snow. All rights reserved.
 //
+//  Copyright (c) 2015 Justin Snow. All rights reserved.
 
 #import <UIKit/UIKit.h>
 
